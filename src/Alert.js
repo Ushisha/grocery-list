e28c9679f50c22a78d732381071b0fc650f20b99
ushisha <usisha.s@gmail.com>
2021-07-10 10:31:39 +0100

    .gitignore is now working

--- src/Alert.js
+++ src/Alert.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
-const Alert =({type,msg,removeAlert,list})=> {
-useEffect(()=>{
-const timeout= setTimeout(()=>{
- removeAlert()
-},3000)
-return ()=> clearTimeout(timeout)
-},[list])
+import React, { useEffect } from "react";
 
-    return <p className={`alert alert-${type}`}>{msg}</p>
-}
+const Alert = ({ type, msg, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [list]);
+
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
 
 export default Alert;
